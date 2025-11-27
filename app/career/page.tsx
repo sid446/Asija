0f7c5b8b28c7d3b7aff903286fb5625e79b83b83
sid446 <162982475+sid446@users.noreply.github.com>
2025-11-27 15:44:03 +0530

    checking

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -97,7 +97,7 @@ export default function CareerPage() {
 
       {/* Job Listings */}
       <div id="openings" className="py-20 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
-        <div className="flex flex-col md:flex-row justify-between items-end mb-12 gap-4">
+        <div className="flex flex-col md:flex-row justify-between items-left  mb-12 gap-4">
           <div>
             <h2 className={`text-3xl md:text-4xl font-bold mb-4 ${isLight ? 'text-slate-900' : 'text-white'}`}>
               Current <span className="text-[#009edb]">Openings</span>
