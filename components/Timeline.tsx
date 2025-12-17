fde5bc608b6a4a027cac6eb554c0e137cedb535e
sid446 <162982475+sid446@users.noreply.github.com>
2025-12-17 15:33:35 +0530

    replaced why choose us on industry and services

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -38,7 +38,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-4xl md:text-6xl mb-6 text-white font-bold max-w-4xl  text-center mx-auto">
+        <h2 className="text-2xl md:text-4xl mb-6 text-white font-bold max-w-4xl  text-center mx-auto">
           Our Journey <span className="text-[#009edb]">.</span>
         </h2>
         <p className="text-gray-300 text-base md:text-lg max-w-2xl leading-relaxed text-center mx-auto mb-8">
@@ -57,7 +57,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-slate-950 border-2 border-[#009edb] flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-[#009edb] border border-[#009edb] p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-gray-400">
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-4xl font-bold text-gray-400">
                 {item.title}
               </h3>
             </div>
