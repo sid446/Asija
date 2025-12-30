8df9579b1e8cbb2b8e199b0d4498fdd5e072da5b
sid446 <162982475+sid446@users.noreply.github.com>
2025-12-30 12:53:43 +0530

    fixed

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider
-      refetchInterval={300}  // Refetch every 5 minutes
+      refetchInterval={false}  // Disable automatic refetching
       refetchOnWindowFocus={false}  // Disable refetch on window focus
     >
       {children}
