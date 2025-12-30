428daa2a273fabe31e7799384a532a3a24220a1b
sid446 <162982475+sid446@users.noreply.github.com>
2025-12-30 17:24:39 +0530

    fixing loading isssue

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
   if (isLoading && !hasData) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <div className="min-h-screen flex items-center justify-center ">
         <Loader />
       </div>
     );
